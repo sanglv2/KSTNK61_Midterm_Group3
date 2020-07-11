a3841e0a1b4f63cdd4dedb04d54167e1f3c58a8d
Sáng Lê Văn <cuahaicang98@gmail.com>
2020-07-11 18:00:41 +0700

    Add quit app action

--- src/View/Management.java
+++ src/View/Management.java
@@ -7,6 +7,7 @@ package View;
 
 import Entity.Manager;
 import java.awt.HeadlessException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -50,8 +51,13 @@ public class Management extends javax.swing.JFrame {
         jMenu2 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setTitle("Quản lý");
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -128,6 +134,11 @@ public class Management extends javax.swing.JFrame {
         jMenu2.setText("Cài đặt");
 
         jMenuItem1.setText("Thoát");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem1);
 
         jMenuBar1.add(jMenu2);
@@ -162,6 +173,14 @@ public class Management extends javax.swing.JFrame {
         new Login().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        quitApplication();
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        quitApplication();
+    }//GEN-LAST:event_formWindowClosing
 
     /**
      * @param args the command line arguments
@@ -215,5 +234,13 @@ public class Management extends javax.swing.JFrame {
 
     private void initManagement() {
         
+    }
+
+    private void quitApplication() {
+        int result = JOptionPane.showConfirmDialog(this, "Bạn có chắc chắn muốn thoát chương trình?", "Xác nhận", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        
+        if (result == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        }
     }
 }
